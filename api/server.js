49c41beb06957f92ea53e9860913153f2b5a7806
Michaiah Bos <michaiahbos@yahoo.com>
2024-02-27 13:02:33 -0600

    wip

--- api/server.js
+++ api/server.js
@@ -3,31 +3,18 @@
 // BREAK UP THIS MONOLITHIC FILE USING ROUTES
 const express = require('express');
 const adoptersRouter = require("./adopters/adopters-router");
+const dogsRouter = require('./dogs/dogs-routes')
 
 const server = express();
 
 server.use(express.json());
 server.use('/api/adopters', adoptersRouter);
+server.use('/api/dogs', dogsRouter)
 
 // const Adopter = require('./adopters/adopters-model');
-const Dog = require('./dogs/dogs-model');
+// const Dog = require('./dogs/dogs-model');
 
 
-
-
-// DOGS ENDPOINTS
-// DOGS ENDPOINTS
-// DOGS ENDPOINTS
-server.get('/api/dogs', (req, res) => {
-  Dog.find()
-    .then(dogs => {
-      res.status(200).json(dogs);
-    })
-    .catch(error => {
-      console.log(error);
-      res.status(500).json({ message: 'Error retrieving the dogs' });
-    });
-});
 
 // OTHER ENDPOINTS
 // OTHER ENDPOINTS
